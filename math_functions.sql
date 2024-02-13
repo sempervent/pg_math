101a850f078489ce01c78b2a1345ceaca8c5f122
sempervent <jngrant9+git@gmail.com>
2024-02-12 21:32:24 -0500

    add more math functions

--- math_functions.sql
+++ math_functions.sql
@@ -2,7 +2,7 @@
 --- @param window_size integer
 --- @param values integer[]
 --- @return integer[]
-CREATE OR REPLACE FUNCTION moving_average(window_size integer, values integer[])
+CREATE OR REPLACE FUNCTION public.moving_average(window_size integer, values integer[])
 RETURNS float[] AS $$
 DECLARE
   result integer[];
@@ -24,7 +24,7 @@ $$ LANGUAGE plpgsql;
 -- Function to calculate the standard deviation
 -- @param values integer[]
 -- @return float
-CREATE OR REPLACE FUNCTION standard_deviation(values integer[])
+CREATE OR REPLACE FUNCTION public.standard_deviation(values integer[])
 RETURNS float AS $$
 DECLARE
   sum integer;
@@ -51,7 +51,7 @@ $$ LANGUAGE plpgsql;
 -- @param value integer
 -- @param values integer[]
 -- @return float
-CREATE OR REPLACE FUNCTION z_score(value integer, values integer[])
+CREATE OR REPLACE FUNCTION public.z_score(value integer, values integer[])
 RETURNS float AS $$
 DECLARE
   mean float;
@@ -67,7 +67,7 @@ $$ LANGUAGE plpgsql;
 -- @param alpha float
 -- @param values integer[]
 -- @return float[]
-CREATE OR REPLACE FUNCTION exponential_moving_average(alpha float, values integer[])
+CREATE OR REPLACE FUNCTION public.exponential_moving_average(alpha float, values integer[])
 RETURNS float[] AS $$
 DECLARE
   result float[];
@@ -85,7 +85,7 @@ $$ LANGUAGE plpgsql;
 -- @param window_size integer
 -- @param values integer[]
 -- @return float[]
-CREATE OR REPLACE FUNCTION relative_strength_index(window_size integer, values integer[])
+CREATE OR REPLACE FUNCTION public.relative_strength_index(window_size integer, values integer[])
 RETURNS float[] AS $$
 DECLARE
   result float[];
@@ -127,7 +127,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- Function to calculate the median
-CREATE OR REPLACE FUNCTION median(values float[])
+CREATE OR REPLACE FUNCTION public.median(values float[])
 RETURNS float AS $$
 DECLARE
   sorted_values float[];
@@ -144,7 +144,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- Function to calculate the mode
-CREATE OR REPLACE FUNCTION mode(values integer[])
+CREATE OR REPLACE FUNCTION public.mode(values integer[])
 RETURNS integer AS $$
 DECLARE
   mode_value integer;
@@ -164,7 +164,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- Function to calculate the range
-CREATE OR REPLACE FUNCTION range(values integer[])
+CREATE OR REPLACE FUNCTION public.range(values integer[])
 RETURNS integer AS $$
 BEGIN
   RETURN max(values) - min(values);
@@ -172,7 +172,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- Function to calculate the nth percentile
-CREATE OR REPLACE FUNCTION percentile(n integer, values float[])
+CREATE OR REPLACE FUNCTION public.percentile(n integer, values float[])
 RETURNS float AS $$
 DECLARE
   sorted_values float[];
@@ -189,7 +189,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- Function to calculate n number of quantiles
-CREATE OR REPLACE FUNCTION quantiles(n integer, values float[])
+CREATE OR REPLACE FUNCTION public.quantiles(n integer, values float[])
 RETURNS float[] AS $$
 DECLARE
   sorted_values float[];
@@ -214,7 +214,7 @@ $$ LANGUAGE plpgsql;
 
 
 -- Function to calculate the covariance
-CREATE OR REPLACE FUNCTION covariance(values1 float[], values2 float[])
+CREATE OR REPLACE FUNCTION public.covariance(values1 float[], values2 float[])
 RETURNS float AS $$
 DECLARE
   mean1 float;
@@ -234,7 +234,7 @@ $$ LANGUAGE plpgsql;
 
 
 -- Function to calculate the correlation
-CREATE OR REPLACE FUNCTION correlation(values1 float[], values2 float[])
+CREATE OR REPLACE FUNCTION public.correlation(values1 float[], values2 float[])
 RETURNS float AS $$
 DECLARE
   std_dev1 float;
@@ -245,5 +245,206 @@ BEGIN
     std_dev2 := (SELECT standard_deviation(values2));
     cov := (SELECT covariance(values1, values2));
     RETURN cov / (std_dev1 * std_dev2);
+END;
+$$ LANGUAGE plpgsql;
+
+--- Function to calculate the simple linear regression
+--- @param x float[]
+--- @param y float[]
+--- @return float[]
+CREATE OR REPLACE FUNCTION public.simple_linear_regression(x float[], y float[])
+RETURNS float[] AS $$
+DECLARE
+  mean_x float;
+  mean_y float;
+  std_dev_x float;
+  std_dev_y float;
+  cov float;
+  slope float;
+  intercept float;
+BEGIN
+    mean_x := (SELECT avg(value) FROM unnest(x) value);
+    mean_y := (SELECT avg(value) FROM unnest(y) value);
+    std_dev_x := (SELECT standard_deviation(x));
+    std_dev_y := (SELECT standard_deviation(y));
+    cov := (SELECT covariance(x, y));
+    slope := cov / (std_dev_x * std_dev_x);
+    intercept := mean_y - slope * mean_x;
+    RETURN ARRAY[slope, intercept];
+END;
+$$ LANGUAGE plpgsql;
+
+-- Function to calculate the variance
+CREATE OR REPLACE FUNCTION public.variance(values float[])
+RETURNS float AS $$
+DECLARE
+  mean float;
+  sum float;
+  i integer;
+BEGIN
+    mean := (SELECT avg(value) FROM unnest(values) value);
+    sum := 0;
+    FOR i IN 1..array_length(values, 1) LOOP
+        sum := sum + (values[i] - mean) * (values[i] - mean);
+    END LOOP;
+    RETURN sum / array_length(values, 1);
+END;
+$$ LANGUAGE plpgsql;
+
+-- Function to calculate the skewness
+CREATE OR REPLACE FUNCTION public.skewness(values float[])
+RETURNS float AS $$
+DECLARE
+  mean float;
+  std_dev float;
+  sum float;
+  i integer;
+BEGIN
+    mean := (SELECT avg(value) FROM unnest(values) value);
+    std_dev := (SELECT standard_deviation(values));
+    sum := 0;
+    FOR i IN 1..array_length(values, 1) LOOP
+        sum := sum + ((values[i] - mean) / std_dev) ^ 3;
+    END LOOP;
+    RETURN sum / array_length(values, 1);
+END;
+$$ LANGUAGE plpgsql;
+
+-- Function to calculate the kurtosis
+CREATE OR REPLACE FUNCTION public.kurtosis(values float[])
+RETURNS float AS $$
+DECLARE
+  mean float;
+  std_dev float;
+  sum float;
+  i integer;
+BEGIN
+    mean := (SELECT avg(value) FROM unnest(values) value);
+    std_dev := (SELECT standard_deviation(values));
+    sum := 0;
+    FOR i IN 1..array_length(values, 1) LOOP
+        sum := sum + ((values[i] - mean) / std_dev) ^ 4;
+    END LOOP;
+    RETURN sum / array_length(values, 1) - 3;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Function to calculate the root mean square
+CREATE OR REPLACE FUNCTION public.root_mean_square(values float[])
+RETURNS float AS $$
+DECLARE
+  sum float;
+  i integer;
+BEGIN
+    sum := 0;
+    FOR i IN 1..array_length(values, 1) LOOP
+        sum := sum + values[i] * values[i];
+    END LOOP;
+    RETURN sqrt(sum / array_length(values, 1));
+END;
+$$ LANGUAGE plpgsql;
+
+-- Function to calculate the geometric mean
+CREATE OR REPLACE FUNCTION public.geometric_mean(values float[])
+RETURNS float AS $$
+DECLARE
+  product float = 1;
+  i integer;
+BEGIN
+    FOR i IN 1..array_length(values, 1) LOOP
+        product := product * values[i];
+    END LOOP;
+    RETURN exp(ln(product) / array_length(values, 1));
+END;
+$$ LANGUAGE plpgsql;
+
+-- Function to calculate the harmonic mean
+CREATE OR REPLACE FUNCTION public.harmonic_mean(values float[])
+RETURNS float AS $$
+DECLARE
+  sum float = 0;
+  i integer;
+BEGIN
+    FOR i IN 1..array_length(values, 1) LOOP
+        sum := sum + (1 / values[i]);
+    END LOOP;
+    RETURN array_length(values, 1) / sum;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Function to calculate the weighted mean
+CREATE OR REPLACE FUNCTION public.weighted_mean(values float[], weights float[])
+RETURNS float AS $$
+DECLARE
+  sum float = 0;
+  sum_weights float = 0;
+  i integer;
+BEGIN
+    FOR i IN 1..array_length(values, 1) LOOP
+        sum := sum + values[i] * weights[i];
+        sum_weights := sum_weights + weights[i];
+    END LOOP;
+    RETURN sum / sum_weights;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Function to calculate the interquartile range
+CREATE OR REPLACE FUNCTION public.interquartile_range(values float[])
+RETURNS float AS $$
+DECLARE
+  q1 float;
+  q3 float;
+BEGIN
+    q1 := percentile(25, values);
+    q3 := percentile(75, values);
+    RETURN q3 - q1;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- Function to calculate the factorial
+CREATE OR REPLACE FUNCTION public.factorial(n integer)
+RETURNS integer AS $$
+BEGIN
+  IF n = 0 THEN
+    RETURN 1;
+  ELSE
+    RETURN n * public.factorial(n - 1);
+  END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Function to calculate the combination
+CREATE OR REPLACE FUNCTION public.combination(n integer, k integer)
+RETURNS integer AS $$
+BEGIN
+  RETURN public.factorial(n) / (public.factorial(k) * public.factorial(n - k));
+END;
+$$ LANGUAGE plpgsql;
+
+-- Function to calculate the permutation
+CREATE OR REPLACE FUNCTION public.permutation(n integer, k integer)
+RETURNS integer AS $$
+BEGIN
+  RETURN public.factorial(n) / public.factorial(n - k);
+END;
+$$ LANGUAGE plpgsql;
+
+--- function to calculate the mean absolute deviation
+--- @param values float[]
+--- @return float
+CREATE OR REPLACE FUNCTION public.mean_absolute_deviation(values float[])
+RETURNS float AS $$
+DECLARE
+  mean float;
+  sum float;
+  i integer;
+BEGIN
+    mean := (SELECT avg(value) FROM unnest(values) value);
+    sum := 0;
+    FOR i IN 1..array_length(values, 1) LOOP
+        sum := sum + abs(values[i] - mean);
+    END LOOP;
+    RETURN sum / array_length(values, 1);
 END;
 $$ LANGUAGE plpgsql;
