9a2a1337439e58e3ecd0a7ee69f7675c8e257357
sempervent <jngrant9+git@gmail.com>
2024-03-13 13:36:37 -0400

    summary stats with percentiles

--- postgis_functions.sql
+++ postgis_functions.sql
@@ -2,7 +2,13 @@
 
 -- zonal statistics
 --- example usage: SELECT * FROM public.get_zonal_statistics();
-CREATE OR REPLACE FUNCTION public.get_zonal_statistics(raster_table text, raster_col text, geom_table text, geom_col text, raster_band integer)
+CREATE OR REPLACE FUNCTION public.get_zonal_statistics(
+    raster_table text,
+    raster_col text,
+    geom_table text,
+    geom_col text,
+    raster_band integer := 1
+)
 RETURNS TABLE (
   id integer,
   count bigint,
@@ -10,30 +16,45 @@ RETURNS TABLE (
   mean double precision,
   stddev double precision,
   min double precision,
-  max double precision
+  max double precision,
+  median double precision,
+  q1 double precision,
+  q3 double precision,
+  q90 double precision,
+  q95 double precision,
+  q99 double precision
 ) AS $$
+DECLARE
+  pixel_values float[];
 BEGIN
   RETURN QUERY EXECUTE format(
-    'SELECT
-      g.id,
-      (stats).count,
-      (stats).sum,
-      (stats).mean,
-      (stats).stddev,
-      (stats).min,
-      (stats).max
-    FROM
-      (
+    'WITH stats AS (
         SELECT
           g.id,
-          ST_SummaryStatsAgg(r.%I, %L, true) as stats
+          ST_SummaryStatsAgg(r.%I, %L, true) as summary_stats,
+          ST_DumpValues(r.%I, %L) as pixel_values
         FROM
           %I g
           INNER JOIN %I r ON ST_Intersects(g.%I, r.rast)
         GROUP BY
           g.id
-      ) as g',
-    raster_col, raster_band, geom_table, raster_table, geom_col
+      )
+      SELECT
+        id,
+        (summary_stats).count,
+        (summary_stats).sum,
+        (summary_stats).mean,
+        (summary_stats).stddev,
+        (summary_stats).min,
+        (summary_stats).max,
+        percentile_cont(0.5) WITHIN GROUP (ORDER BY unnest(pixel_values)) as median,
+        percentile_cont(0.25) WITHIN GROUP (ORDER BY unnest(pixel_values)) as q1,
+        percentile_cont(0.75) WITHIN GROUP (ORDER BY unnest(pixel_values)) as q3,
+        percentile_cont(0.9) WITHIN GROUP (ORDER BY unnest(pixel_values)) as q90,
+        percentile_cont(0.95) WITHIN GROUP (ORDER BY unnest(pixel_values)) as q95,
+        percentile_cont(0.99) WITHIN GROUP (ORDER BY unnest(pixel_values)) as q99
+      FROM stats',
+    raster_col, raster_band, raster_col, raster_band, geom_table, raster_table, geom_col
   );
 END;
 $$ LANGUAGE plpgsql;
